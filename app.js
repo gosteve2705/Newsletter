e30dcf04438172c092b5d1259da169b925aa8aa8
gosteve <nomuojamaro@gmail.com>
2020-06-04 07:00:29 +0100

    ready for depoyment

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.post('/failure',function(req,res){
 
 
 
-app.listen(3000, function (){
+app.listen(process.env.PORT || 3000, function (){
   console.log('server is up and running on port 3000');
 });
